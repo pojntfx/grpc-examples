141837fff9f254f4d071a652213570b8ffda40c8
Felix Pojtinger <felix@pojtinger.com>
2021-06-07 22:12:57 +0000

    feat: Add Rust example

---
+++ mather-rs/src/main.rs
@@ -0,0 +1,44 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use mather_rs::mather_server::{Mather, MatherServer};
+use mather_rs::{AddInputMessage, AddOutputMessage};
+
+pub mod mather_rs {
+    tonic::include_proto!("com.pojtinger.felix.grpc_examples.gomather");
+}
+
+#[derive(Debug, Default)]
+pub struct MyMather {}
+
+#[tonic::async_trait]
+impl Mather for MyMather {
+    async fn add(
+        &self,
+        request: Request<AddInputMessage>,
+    ) -> Result<Response<AddOutputMessage>, Status> {
+        let aim = request.into_inner();
+
+        let reply = mather_rs::AddOutputMessage {
+            sum: aim.first_summand + aim.second_summand,
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let laddr = std::env::var("LADDR")
+        .unwrap_or("0.0.0.0:5000".to_string())
+        .parse()?;
+    let mather = MyMather::default();
+
+    println!("Listening on {}", laddr);
+
+    Server::builder()
+        .add_service(MatherServer::new(mather))
+        .serve(laddr)
+        .await?;
+
+    Ok(())
+}
